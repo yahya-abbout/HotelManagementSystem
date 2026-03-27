29008620f3f754887ab50830c32817ad31cfe773
Yahya <yahya.abbout.a24@gmail.com>
2026-03-28 01:29:34 +0200

    Refactor Admin class for better customer management
    Refactored Admin class to use HashMap for customers and added waiting list functionality.

--- HotelManagementSystem/src/Admin.java
+++ HotelManagementSystem/src/Admin.java
@@ -1,35 +1,62 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.LinkedList;
+
 
 public class Admin {
 
+    HashMap<Integer, Customer> customers = new HashMap<>();
+    HashSet<Integer> usedIds = new HashSet<>();
+    Queue<Customer> waitingList = new LinkedList<>();
 
-ArrayList<Customer> customers;
 
-    public Admin(ArrayList<Customer> customers) {
-
-        this.customers = customers;
+    public Admin() {
     }
 
-    public void AddCostumer(String name, String LastName, int id, int plan, double TotalPrice){
-        customers.add(new Customer(name, LastName, id, plan, TotalPrice));
-        System.out.println("Costumer added");
-    }
-    public void DeleteCostumer(String name, String LastName, int id, int plan, double TotalPrice){
-        for(int i=0;i<customers.size();i++){
-            if (customers.get(i).getId() == id){
-                customers.remove(i);
-                System.out.println("costumer deleted");
-                return;
-            }
+    
+    public void AddCostumer(String name, String LastName, int id, int plan, double TotalPrice) {
+
+        
+        if (usedIds.contains(id)) {
+            System.out.println("Error: Customer ID " + id + " already exists!");
+            return;
         }
-        System.out.println("costumer not found");
 
-
+        Customer newCustomer = new Customer(name, LastName, id, plan, TotalPrice);
+        customers.put(id, newCustomer);
+        usedIds.add(id);
+        newCustomer.addBooking("Plan " + plan + " - $" + TotalPrice);
+        System.out.println("Customer added successfully.");
     }
 
-    public void loginAdmin(String name, int id) {
+  
+    public void DeleteCostumer(int id) {
 
-
+        
+        if (customers.containsKey(id)) {
+            customers.remove(id);
+            usedIds.remove(id);
+            System.out.println("Customer deleted.");
+        } else {
+            System.out.println("Customer not found.");
+        }
     }
+
+    public void addToWaitingList(Customer c) {
+        waitingList.offer(c);
+        System.out.println(c.name + " added to waiting list. Position: " + waitingList.size());
+    }
+
+    public void serveNextWaiting() {
+        Customer next = waitingList.poll();
+        if (next != null) {
+            System.out.println("Serving next customer: " + next.name);
+            AddCostumer(next.name, next.getLastName(), next.getId(), next.plan, next.getTotalPrice());
+        } else {
+            System.out.println("Waiting list is empty.");
+        }
+    }
+
+    public void loginAdmin(String name, int id) {}
 }
-
