98fe44da33189bc717f5f51cf5271a45241872fd
Yahya <yahya.abbout.a24@gmail.com>
2026-03-29 01:16:58 +0200

    Enhance Customer class with booking history
    Added booking history functionality to Customer class.

--- HotelManagementSystem/src/Customer.java
+++ HotelManagementSystem/src/Customer.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
     String name;
     private String LastName;
     private int id;
     int plan;
     private double TotalPrice;
+
+    private List<String> bookingHistory = new ArrayList<>();
 
 
     public Customer(String name, String LastName, int id, int plan, double TotalPrice) {
@@ -13,14 +18,24 @@ public class Customer {
         this.plan = plan;
         this.TotalPrice = TotalPrice;
     }
-    public int getId(){
+
+    public int getId() {
         return id;
     }
-    public String getLastName(){
+
+    public String getLastName() {
         return LastName;
     }
-    public double getTotalPrice(){
+
+    public double getTotalPrice() {
         return TotalPrice;
     }
 
+    public void addBooking(String bookingDetails) {
+        bookingHistory.add(bookingDetails);
+    }
+
+    public List<String> getBookingHistory() {
+        return bookingHistory;
+    }
 }
